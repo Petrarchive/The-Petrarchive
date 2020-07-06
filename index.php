f63d4c78697da755065e23da7af909be9bd8e389
Isabella <isamagni@indiana.edu>
2020-07-05 23:01:18 -0400

    Update index.php
    Added Italics to Petrarchive, for consistency

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
   <link rel="stylesheet" type="text/css" href="js/jquery-ui/jquery-ui.min.css" />
 
   <link rel="stylesheet" type="text/css" href="css/stylesheets/screen.css" />
-  <link href="css/custom.css" media="screen, projection" rel="stylesheet" type="text/css" /> 
-  <link href="css/auxillaryPage.css" media="screen, projection" rel="stylesheet" type="text/css" />       
+  <link href="css/custom.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="css/auxillaryPage.css" media="screen, projection" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" type="text/css" href="css/stylesheets/index.css" />
   <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165728171-1"></script>
@@ -41,9 +41,9 @@
     <div id="bannertitle">Petr<span class="archive">archive</span></div>
     <div id="bannersubtitle">An edition of Petrarch’s songbook</div>
     <div id="bannerrvf">Rerum vulgarium fragmenta</div>
-    <div id="bannerresp">Edited by 
-      <a href="http://frit.indiana.edu/faculty/storey.shtml" target="_blank">H. Wayne Storey</a>, 
-      <a href="http://info.ils.indiana.edu/~jawalsh/" target="_blank">John A. Walsh</a> and 
+    <div id="bannerresp">Edited by
+      <a href="http://frit.indiana.edu/faculty/storey.shtml" target="_blank">H. Wayne Storey</a>,
+      <a href="http://info.ils.indiana.edu/~jawalsh/" target="_blank">John A. Walsh</a> and
       <a href="http://isamagni.com/" target="_blank">Isabella Magni</a>
     </div>
   </div>
@@ -52,9 +52,9 @@
     <div id="bannertitle">Petr<span class="archive">archive</span></div>
     <div id="bannersubtitle">An edition of Petrarch’s songbook</div>
     <div id="bannerrvf">Rerum vulgarium fragmenta</div>
-    <div id="bannerresp">Edited by 
-      <a href="http://frit.indiana.edu/faculty/storey.shtml" target="_blank">H. Wayne Storey</a>, 
-      <a href="http://info.ils.indiana.edu/~jawalsh/" target="_blank">John A. Walsh</a> and 
+    <div id="bannerresp">Edited by
+      <a href="http://frit.indiana.edu/faculty/storey.shtml" target="_blank">H. Wayne Storey</a>,
+      <a href="http://info.ils.indiana.edu/~jawalsh/" target="_blank">John A. Walsh</a> and
       <a href="http://isamagni.com/" target="_blank">Isabella Magni</a>
     </div>
   </div>
@@ -69,24 +69,24 @@
 <section class="row justify-content-center">
   <main class="col-lg-8 col-md-12">
     <p class="">
-      When you hear the words <em>sonnet</em>, <em>Renaissance</em>, 
-      <em>western literature</em>, <em>love songs</em>, 
-      you are hearing the direct influence of Francesco Petrarca 
-      (Petrarch) and of his collection of lyric poems that he called 
-      “Fragments in the Vernacular” (<cite>Rerum vulgarium 
+      When you hear the words <em>sonnet</em>, <em>Renaissance</em>,
+      <em>western literature</em>, <em>love songs</em>,
+      you are hearing the direct influence of Francesco Petrarca
+      (Petrarch) and of his collection of lyric poems that he called
+      “Fragments in the Vernacular” (<cite>Rerum vulgarium
       fragmenta</cite>).
     </p>
 
     <p class="">
-      The Petrarchive is designed as a tool both to introduce Petrarch’s 
-      collection—a collection that continues to influence modern cultures 
-      in many languages—and to give advanced users access to 
-      Petrarch’s “original” text and an extensive “material” 
-      commentary for each poem. Within the Petrarchive you will find 
-      edited and diplomatic transcriptions of Petrarch’s songbook; 
-      visual maps illustrating Petrarch’s design, layout, and 
-      visual poetics; and multi-layered commentary addressing prosodic, historic, 
-      textual, and thematic issues along with an English translation 
+      The Petr<i>archive</i> is designed as a tool both to introduce Petrarch’s 
+      collection—a collection that continues to influence modern cultures
+      in many languages—and to give advanced users access to
+      Petrarch’s “original” text and an extensive “material”
+      commentary for each poem. Within the Petrarchive you will find
+      edited and diplomatic transcriptions of Petrarch’s songbook;
+      visual maps illustrating Petrarch’s design, layout, and
+      visual poetics; and multi-layered commentary addressing prosodic, historic,
+      textual, and thematic issues along with an English translation
       of the poems.
     </p>
   </main>
