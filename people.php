99793dcd0f710ba571a00aee78cf2adfecd300d3
isamagni <magni.isabella@gmail.com>
2023-02-23 14:28:29 +0000

    Updated People to include new affiliation and Homepage for Isabella
    Updated People page to include:
    new affiliation/bio/Homepage for Isabella
    deleted duplicate Marco Aresu
    updated title for Paolo, Maria Teresa and Giulia

--- people.php
+++ people.php
@@ -34,28 +34,28 @@
           <div id="content" class="col">
             <h1>People</h1>
             <dl>
-            <dt>H. Wayne Storey, Editor</dt>
+            <dt>H. Wayne Storey, Editor and Co-Principal Investigator</dt>
             <dd>H. Wayne Storey is Professor Emeritus of Italian and Medieval Studies at Indiana University, Bloomington. He has researched and published extensively on digital and material philology, textual editing, Medieval and Renaissance Italian and Old Occitan literatures. Among his notable publications are <em>Transcription and Visual Poetics in the Early Italian Lyric</em> (Garland Press, 1993), Rerum vulgarium fragmenta: <em>Facsimile del codice autografo Vaticano Latino 3195</em> (Antenore, 2003-2004) and <em>Petrarch and the Textual Origins of Interpretation</em> (Brill, 2007). Storey is President Society for Textual Scholarship
 						<br/>
 						Homepage: <a href="https://frit.indiana.edu/about/emeriti-faculty/storey-h.html">https://frit.indiana.edu/about/emeriti-faculty/storey-h.html</a></dd>
-            <dt>John A. Walsh, Editor</dt>
+            <dt>John A. Walsh, Editor and Co-Principal Investigator</dt>
 <dd>
 John A. Walsh is the Director of the <a href="https://www.hathitrust.org/htrc">HathiTrust Research Center</a> and Associate Professor of <a href="http://ils.indiana.edu">Information and Library Science</a> in the <a href="http://sice.indiana.edu">Luddy School of Informatics, Computing, and Engineering</a> at Indiana University. His research applies computational methods to the study of literary and historical documents. Walsh is an editor of digital scholarly editions, including: the <a href="http://petrarchive.org">Petrarchive</a>, the <a href="http://swinburneproject.org">Algernon Charles Swinburne Project</a>, and the <a href="http://chymistry.org">Chymistry of Isaac Newton</a>. He developed <a href="http://cbml.org">Comic Book Markup Language</a> (CBML) for scholarly encoding of comics and graphic novels, and <a href="http://teiboilerplate.org">TEI Boilerplate</a>, for publishing documents encoded according to the Text Encoding Initiative (TEI) <a href="http://www.tei-c.org/release/doc/tei-p5-doc/en/html/index.html">Guidelines for Electronic Text Encoding and Interchange</a>. He is the founding and current Technical Editor of <a href="http://digitalhumanities.org/dhq">Digital Humanities Quarterly</a>, an open-access online journal published by the <a href="http://adho.org">Alliance of Digital Humanities Organizations</a>. Walsh’s research interests include: computational literary studies; textual studies and bibliography; text technologies; book history; 19th-century British literature, poetry and poetics; and comic books. <br />Homepage: <a href="http://johnwalsh.name">http://johnwalsh.name</a>.
             </dd>
             </dl>
             <dl>
-            <dt>Isabella Magni, Editor</dt>
-            <dd>Isabella Magni is Postdoctoral Fellow at the HathiTrust Research Center (Luddy School of Informatics, Indiana University) where she manages and collaborates with five digital humanities projects that tell the story of historically under-resourced and marginalized textual communities (<a href="https://wiki.htrc.illinois.edu/pages/viewpage.action?pageId=87785594">SCWAReD</a> projects). In addition to her work as Co-Editor of the Petrarchive, Isabella is also Editor of <a href="https://italian-paleography.library.utoronto.ca">Italian Paleography</a> (with Lia Markey and Maddalena Signorini). She teaches and conducts research in digital humanities and digital philology, Medieval and Early modern literature, history of the book, Petrarch and Dante. She recently co-edited a volume on <a href="https://brill.com/view/title/58363">Interpretation and visual poetics in Medieval and Early Modern texts</a> (2021), edited a special issue on <a href="https://emdr.itercommunity.org/index.php/emdr/issue/view/8">Digital Paleography</a> for the Early Modern Digital Review journal (2020) and serves as digital editor for <a href="http://textual-cultures.org/">Textual Cultures</a> journal. She is currently working on a new digital edition of the <a href="http://isamagni.com/?page_id=228#albizzi ">Albizi Memorial Book</a> and a monograph on forms and textualities of Medieval memorial books.<br/>
-						Before joining HTRC and Indiana University, Isabella was Postdoctoral Associate in Italian and Digital Humanities at Rutgers University (2019-2021) and Mellon Postdoctoral Fellow in Digital Paleography at the Newberry Library in Chicago (2017-2019). She earned her PhD in Italian and Medieval Studies at Indiana University (2017).
+            <dt>Isabella Magni, Editor and Co-Principal Investigator</dt>
+            <dd>Isabella Magni is Lecturer in Digital Humanities at the University of Sheffield (Sheffield, UK). In addition to her work as Editor of the Petrarchive, Isabella is also Co-Investigator of the <a href="https://wiki.htrc.illinois.edu/pages/viewpage.action?pageId=87785594">SCWAReD</a> project in collaboration with the HathiTrust Research Center, and Editor of <a href="https://italian-paleography.library.utoronto.ca">Italian Paleography</a> (with Lia Markey and Maddalena Signorini). She teaches and conducts research in digital humanities and digital philology, Medieval and Early modern literature, history of the book, Petrarch and Dante. She co-edited a volume on <a href="https://brill.com/view/title/58363">Interpretation and visual poetics in Medieval and Early Modern texts</a> (2021), edited a special issue on <a href="https://emdr.itercommunity.org/index.php/emdr/issue/view/8">Digital Paleography</a> for the Early Modern Digital Review journal (2020) and serves as digital editor for <a href="http://textual-cultures.org/">Textual Cultures</a> journal. She is currently working on a new digital edition of the <a href="http://isamagni.com/?page_id=228#albizzi ">Albizi Memorial Book</a> and a monograph on forms and textualities of Medieval memorial books.<br/>
+						Before joining the University of Sheffield, Isabella was Postdoctoral Fellow at HTRC and Indiana University (2021-2022); Postdoctoral Associate in Italian and Digital Humanities at Rutgers University (2019-2021); and Mellon Postdoctoral Fellow in Digital Paleography at the Newberry Library in Chicago (2017-2019). She earned her PhD in Italian and Medieval Studies at Indiana University (2017).
             <br/>
-            Homepage: <a href="http://isamagni.com">http://isamagni.com</a>.<br/>
+            Homepage: <a href="https://www.sheffield.ac.uk/dhi/about-dhi/people/isabella-magni">https://www.sheffield.ac.uk/dhi/about-dhi/people/isabella-magni</a>.<br/>
             </dd>
             <dt>Francesco Marco Aresu, Editor</dt>
-            <dd/>
+            <dd>Homepage: <a href="https://www.wesleyan.edu/academics/faculty/faresu/profile.html">https://www.wesleyan.edu/academics/faculty/faresu/profile.html</a></dd>
             </dl>
 
 						<dl>
-            <dt>Maria Teresa De Luca, Encoder and Research Assistant<!-- 2020-present --></dt>
+            <dt>Maria Teresa De Luca, Associate Editor and Encoder<!-- 2020-present --></dt>
 						<dd>Maria Teresa De Luca is a Ph.D. student in Italian at Rutgers University and adjunct lecturer in Italian at Columbia University. Her main research focuses on narrative theory and history of the novel, and their intersection with Italian linguistics and theory of language in the twentieth and twenty-first century, along with standing interests in Medieval Italian literature and language, especially Dante’s understanding of the nature and limits of language.
 <br/>
 She has extensive experience working on the main Italian etymological and historical dictionaries (LEI - <cite>Lessico Etimologico Italiano</cite>, DI - <cite>Deonomasticon Italicum</cite> and TLIO - <cite>Tesoro della Lingua Italiana delle Origini</cite>). Maria Teresa is also working on the reconstruction of the editorial history of manuscript Ashb. 956, one of the very few southern Italian vernacular translations of the <cite>Liber abaci</cite> by Leonardo Pisano, and a strategic linguistic and lexical source for understanding southern Italian culture of the Middle Ages. She is co-editor of the newly founded Digital Humanities project <cite>Divine Networks: An Interactive Visualization of Dante’s Comedy</cite>. She previously studied at the Universität des Saarlandes where she obtained a Ph.D. in Italian linguistics. Her dissertation, ‘La terminologia linguistica in <cite>Lingua nostra</cite> (1939-1978)’ was awarded the premio Giovanni Nencioni by the Accademia della Crusca.<br/>
@@ -64,33 +64,26 @@ Homepage: <a href="https://italian.rutgers.edu/people/phd-students/115-departmen
             </dl>
 
 						<dl>
-            <dt>Paolo Scartoni, Encoder and Research Assistant<!-- 2020-present --></dt><dd />
+            <dt>Paolo Scartoni, Chief Associate Editor and Encoder<!-- 2020-present --></dt><dd />
 						<dd>Paolo Scartoni is a Ph.D. student at Rutgers University. His research focuses on the relationship between musica speculativa and grammar in Dante’s linguistics and poetics. In addition to collaborate as an encoder at Petr<em>archive</em>, he is also working as a contributor at the Deiphira Project, the digital edition of a witness of Alberti's Deiphira (Harvard University's Houghton Library, MS Typ. 422). Prior to joining Rutgers, he earned a M.M. from the Conservatory of Perugia (Italy) where he graduated in Historical Piano and defended a thesis on the role of literature in Robert Schumann’s music for piano.
 						<br/>
 						Homepage: <a href="https://italian.rutgers.edu/people/phd-students/115-departmental-info/people/phd-graduate-students/377-scartoni-paolo">https://italian.rutgers.edu/people/phd-students/115-departmental-info/people/phd-graduate-students/377-scartoni-paolo</a></dd>
             </dl>
 
 						<dl>
-						<dt>Marco Aresu, Textual Analyst<!-- 2020-present --></dt><dd />
-						<dd>Homepage: <a href="https://www.wesleyan.edu/academics/faculty/faresu/profile.html">https://www.wesleyan.edu/academics/faculty/faresu/profile.html</a></dd>
-						</dl>
-
-						<dl>
-						<dt>Giulia Benghi, Textual Analyst<!-- 2020-present --></dt><dd />
+						<dt>Giulia Benghi, Associate Editor and Encoder<!-- 2020-present --></dt><dd />
 						<dd>Giulia Benghi received her Ph.D. in Italian and Manuscript Studies at Indiana University in 2020, with a dissertation that examines the material and philological conditions of the codex Cologny, Bodmer 131. Her main research interests include manuscript tradition and literature in fourteenth- and fifteenth-century Italy, material philology, codicology, and textual editing. She presented on <em>Boccaccio’s Scholarship and His Copy Method</em> at the 2013 Medieval Studies Conference in Kalamazoo and on the codex Cologny Bodmer 131 at the 2015 Binghamton Conference on Italian Songbooks. Her essay on the transcriptional habits employed by scribes for the five genres of Petrarch’s RVF in the late fourteenth century appeared in the journal “Textual Cultures” in Spring 2020, issue 13:1, titled <em>Transcribing Petrarch’s Genres in the Late Fourteenth Century. An Ongoing Conversation with the Observations from MMS Cologny Bodmer 131 and Gambalunga SC-Ms. 93</em>.</dd>
 						</dl>
 
-            <dl>
-            <dt>Abraham Kim, Web Developer <!-- 2017-present --></dt><dd />
+
+            <h2>Past Contributors</h2>		
+						<dl>
+            <dt>Abraham Kim, Web Developer (2017-2020) <!-- 2017-present --></dt><dd />
             </dl>
-
-
-
-            <h2>Past Contributors</h2>
-						<dl>
-						<dt>Morgan Anderson, Encoder (2019-2020)<!-- 2019-present --></dt><dd />
-						</dl>
-						<dl>
+			<dl>
+			<dt>Morgan Anderson, Encoder (2019-2020)<!-- 2019-present --></dt><dd />
+			</dl>
+			<dl>
             <dt>Rebecca Parker, Encoder (2018)</dt><dd />
             </dl>
             <dl>
@@ -114,7 +107,6 @@ Homepage: <a href="https://italian.rutgers.edu/people/phd-students/115-departmen
             <dt>Laura Pence, Visual Index Designer (2013-2014)<!-- 2014-present --></dt><dd />
             </dl>
             <dl>
-            <dt>Sara Dallavalle, Encoder (2014)</dt><dd />
 
             <dt>Allison M. McCormack, Encoder (2013-14)</dt>
             <dd>Allison McCormack is the Rare Books Catalog Librarian at <a href="http://www.baylor.edu">Baylor University</a> in Waco, Texas. Trained as a medievalist, her work at Baylor involves researching and cataloging rare books, archival collections, and other special collections materials in such diverse areas as historical Texana, Soviet samizdat publications, modern artists’ books, and medieval music manuscripts. She regularly collaborates with Baylor Classics professor <a href="http://www.baylor.edu/classics/index.php?id=89572">Daniel Nodes</a> on a variety of Latin-language projects, including most recently a transcription of the 1542 commentary of Alexius Vanegas of Toledo on Petrus Papeus’ Comoedia de Samaritano Evangelico, as well as the transcription and translation of a series of manuscript sermon notes known as the Collationes of Frater Petrus. <br/>
