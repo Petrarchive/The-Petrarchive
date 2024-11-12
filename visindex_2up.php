b83f3bcd805844c137d47e9073f337a71282ff17
jawalsh <jawalsh@indiana.edu>
2024-11-12 10:12:06 -0500

    Update visindex_2up.php
    Added hack to get 1r to display in on right-hand side of two-column layout.

--- visindex_2up.php
+++ visindex_2up.php
@@ -62,7 +62,7 @@
         </header>
 
 		<div id="content">
-		<h1 style="margin-top: 5em">A visual index to Petrarch's <cite>Rerum vulgarium fragmenta</cite>, Vat. Lat. 3195</h1>
+		<h1 style="margin-top: 5em; margin-left:9.56rem;">A visual index to Petrarch's <cite>Rerum vulgarium fragmenta</cite>, Vat. Lat. 3195</h1>
 		<div id="visindex">
 		<div style="margin-bottom:5px;">
 			<a href="content/c001r.xml"><img alt="vat. lat. 3195, c. 1r" class="visindex-recto" src="images/visindex/c001r.svg" id="c001r" /></a>
